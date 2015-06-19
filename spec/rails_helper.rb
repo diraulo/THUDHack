b19704e011928646f6102e9282adfc4b014c0525
Raoul DIFFOUO <diraulo@gmail.com>
2015-06-19 22:09:54 +0200

    Configure cucumber and test environment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,22 +1,56 @@
-ENV["RAILS_ENV"] = "test"
+require 'webmock/rspec'
 
-require File.expand_path("../../config/environment", __FILE__)
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+require 'shoulda/matchers'
 
-require "rspec/rails"
-require "shoulda/matchers"
-
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |file| require file }
-
-module Features
-  # Extend this module in spec/support/features/*.rb
-  include Formulaic::Dsl
-end
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |file| require file }
 
 RSpec.configure do |config|
   config.include Features, type: :feature
   config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.clean_with(:truncation)
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = RSpec.current_example.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.include Capybara::DSL
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  # config.mock_with :rspec do |mocks|
+  #   mocks.syntax = :expect
+  #   mocks.verify_partial_doubles = true
+  # end
+  #
+  # config.order = :random
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
 
 ActiveRecord::Migration.maintain_test_schema!
