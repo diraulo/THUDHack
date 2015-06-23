442eaeabb1f87fcdb68cdbaa52a80199ff76bdd2
Joao Nzango <joaonzangoii@hotmail.com>
2015-06-23 19:10:34 +0200

    fix gem file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
   provider :linked_in, ENV['LINKED_IN_KEY'], ENV['LINKED_IN_SECRET']
 end
