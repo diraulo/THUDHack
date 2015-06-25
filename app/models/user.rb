2f9b8b1628813ef1d17111cfbcf910b922e10e9a
Raoul DIFFOUO <diraulo@gmail.com>
2015-06-25 21:20:58 +0200

    update omniauth configs for google

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,25 +2,26 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :timeoutable,:omniauthable, :omniauth_providers => [:google_oauth2,:linkedin,:twitter]
+         :confirmable, :lockable, :timeoutable, :omniauthable,
+         omniauth_providers: [:google_oauth2, :linkedin]
 
   MEMBER_TYPES = ['Entrepreneur', 'Event coordinator/Chapter owner',
                   'Administrator', 'Partner/Sponsor']
   validates_presence_of :first_name, :last_name, :city, :member_type
   validates_acceptance_of :t_and_cs
 
-
   def self.from_omniauth(access_token)
-      data = access_token.info
-      user = User.where(:email => data["email"]).first
-      unless user
-          user = User.create(name: data["name"],
-             email: data["email"],
-             password: Devise.friendly_token[0,20]
-          )
-      end
-      user
-      
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    unless user
+      user = User.create(
+        name: data['name'],
+        email: data['email'],
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+
+    user
   end
 end
-
